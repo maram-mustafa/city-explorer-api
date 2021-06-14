1c404d127aa37a965f484c3617f2e34d79ad421e
maram-mustafa <maram.w.mustafa@gmail.com>
2021-06-14 17:06:52 +0300

    backend

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const server = express();
+const weatherData = require("./assets/weather.json");
+
 // const cors = require("cors");
-// const weatherData = require("./assets/weather.json");
 
 const PORT = 3010;
 //to specify Root Route
@@ -10,9 +11,26 @@ server.get("/", (req, res) => {
   res.send("test route");
 });
 
+//localhost:3010/weatherAll
+server.get("/weatherAll", (req, res) => {
+    res.send(weatherData);
+  });
 
+//localhost:3010/weather?lat=..&lon=..&searchQuery=...
+server.get("/weather", (req, res) => {
+    let lat = req.query.lat;
+    let lon = req.query.lon;
+    let searchQuery = req.query.searchQuery;
 
+    let result =''
+    if(lat == weatherData.lat && lon == weatherData.lon && searchQuery == weatherData.city_name){
+        result = weatherData.data
+    }else{
+        result = 'error'
+    }
 
+    res.send(result)
+});
 
 server.listen(PORT, () => {
   console.log(`listen on port ${PORT}`);
