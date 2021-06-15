048302623def627d30606e158f8c80cd96779d38
maram-mustafa <maram.w.mustafa@gmail.com>
2021-06-15 19:48:14 +0300

    fix

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ server.get("/weather", (req, res) => {
 
   class Data {
     constructor(item) {
-      (this.data = item.datetime),
+      (this.data = item.snow),
         (this.description = item.weather.description);
     }
   }
