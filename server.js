3b027c5968b787051382b42a8689113946c17386
maram-mustafa <maram.w.mustafa@gmail.com>
2021-06-16 00:28:16 +0300

    fix

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const weatherData = require("./assets/weather.json");
 const cors = require("cors");
+const axios = require("axios");
+const { response } = require("express");
 const server = express();
 
 server.use(cors());
 
 const PORT = process.env.PORT;
+
+class Data {
+  constructor(item) {
+    (this.datetime = item.datetime),
+      (this.description = item.weather.description);
+  }
+}
+
 //to specify Root Route
 //localhost:3010/
 server.get("/", (req, res) => {
@@ -18,24 +28,39 @@ server.get("/weatherAll", (req, res) => {
   res.send(weatherData);
 });
 
+server.get("/weather2", (req, res) => {
+  const key = process.env.WEATHER_KEY;
+  let searchQuery = req.query.searchQuery;
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery}&key=${key}`;
+
+  // class Data {
+  //   constructor(item) {
+  //     (this.datetime = item.datetime),
+  //       (this.description = item.weather.description);
+  //   }
+  // }
+
+  
+  axios.get(url).then((response) => {
+    console.log(response.data);
+
+    let result = response.data.data.map((item) => {
+      return new Data(item);
+    });
+    res.send(result);
+  });
+});
+
 //localhost:3010/weather?lat=..&lon=..&searchQuery=...
 server.get("/weather", (req, res) => {
   // let lat = req.query.lat;
   // let lon = req.query.lon;
   let searchQuery = req.query.searchQuery;
 
-  class Data {
-    constructor(item) {
-      (this.datetime = item.datetime),
-        (this.description = item.weather.description);
-    }
-  }
-
   let result = weatherData
     .find((item) => {
       if (searchQuery == item.city_name) {
         return item;
-        ``;
       }
     })
     .data.map((item) => {
