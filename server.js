2ebb25bf6b19ca7293e9dbc5f8e080230311249b
maram-mustafa <maram.w.mustafa@gmail.com>
2021-06-16 02:09:23 +0300

    fix

--- server.js
+++ server.js
@@ -40,7 +40,6 @@ server.get("/weather2", (req, res) => {
   //   }
   // }
 
-  
   axios.get(url).then((response) => {
     console.log(response.data);
 
@@ -68,6 +67,31 @@ server.get("/weather", (req, res) => {
     });
   res.send(result);
 });
+
+class MovieData {
+  constructor(item){
+    this.title = item.title,
+    this.img = `https://image.tmdb.org/t/p/w500${item.poster_path}`
+  }
+}
+
+server.get("/movies", (req, res) => {
+  //https://api.themoviedb.org/3/discover/movie?api_key=${key}&region=${cityName}
+
+  const key = process.env.MOVIE_KEY;
+  let cityName = req.query.searchQuery;
+  let url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&region=${cityName}`;
+
+  axios.get(url).then((response) => {
+    console.log(response);
+      let result = response.data.results.map((item) => {
+        return new MovieData(item)
+      });
+      res.send(result);
+  });
+});
+
+
 
 server.get("*", (req, res) => {
   res.status(404).send("sorry, this page not found");
